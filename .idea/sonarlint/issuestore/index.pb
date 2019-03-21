98c5d7976dda2bb6be8856d5eb994b17a3244328
Goncharov01 <mr.sacha.goncharov.01@mail.ru>
2019-03-21 23:34:03 +0300

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+s
+Capp/src/main/java/com/example/googlepodcastsfinal/MainActivity.java,8\d\8d2f0c8abfb6032814eb5d20ce3f60df617596f9
---
+++ app/src/main/java/com/example/googlepodcastsfinal/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.googlepodcastsfinal;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+
+}
